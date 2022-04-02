c4241da3711992d45e774203a4fa427512cf5050
Hunter <hunter20018@gmail.com>
2022-04-02 14:28:32 -0500

    Added typedef for AddressInfo_t

--- Util/Util.h
+++ Util/Util.h
@@ -12,10 +12,12 @@ typedef int SFD_t; /**< Socket File Descriptor*/
 
 typedef int Protocol_t; /**< Protocol (TCP, UDP, etc.)*/
 
-typedef struct sockaddr Address_t; /**< Socket Address*/
+typedef struct sockaddr SocketAddress_t; /**< Socket Address*/
 
 typedef int SocketType_t; /**< Socket Type (SOCK_STREAM, SOCK_DGRAM)
 							Note that this will usually be automatically set when setting a protocol(Protocol_t)*/
+
+typedef struct addrinfo AddressInfo_t;
 
 #define PROTOCOL_TCP IPPROTO_TCP /**< Defines IPPROTO_TCP as PROTOCOL_TCP. This can be used for a Protocol_t*/
 #define PROTOCOL_UDP IPPROTO_UDP /**< Defines IPPROTO_UDP as PROTOCOL_UDP. This can be used for a Protocol_t*/
